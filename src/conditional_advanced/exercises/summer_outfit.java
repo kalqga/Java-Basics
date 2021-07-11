96e72b4d52262fe950000afd3e8e58977090149a
kalqga <kalqga123@gmail.com>
2021-07-11 11:31:31 +0300

    10.07.21

---
+++ src/conditional_advanced/exercises/summer_outfit.java
@@ -0,0 +1,56 @@
+package conditional_advanced.exercises;
+
+import java.util.Scanner;
+
+public class summer_outfit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int degrees = Integer.parseInt(scan.nextLine());
+        String time_of_day = scan.nextLine();
+        String outfit = "";
+        String shoes = "";
+
+        switch (time_of_day) {
+            case "Morning":
+                if (degrees >= 10 && degrees <= 18) {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                } else if (degrees > 18 && degrees <= 24) {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                } else if (degrees >= 25) {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                break;
+            case "Afternoon":
+                if (degrees >= 10 && degrees <= 18) {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                } else if (degrees > 18 && degrees <= 24) {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                } else if (degrees >= 25) {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                break;
+            case "Evening":
+                if (degrees >= 10 && degrees <= 18) {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                } else if (degrees > 18 && degrees <= 24) {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                } else if (degrees >= 25) {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                break;
+        }
+        System.out.printf("It's %d degrees, get your %s and %s.", degrees, outfit, shoes);
+
+
+    }
+}
