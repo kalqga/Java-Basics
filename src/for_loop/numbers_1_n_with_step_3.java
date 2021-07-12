a9463eb134cb12a3a23b683f52be44124c0704bf
kalqga <kalqga123@gmail.com>
2021-07-12 13:26:04 +0300

    12.07.21

---
+++ src/for_loop/numbers_1_n_with_step_3.java
@@ -0,0 +1,16 @@
+package for_loop;
+
+import java.util.Scanner;
+
+public class numbers_1_n_with_step_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= n; i += 3) {
+            System.out.println(i);
+        }
+
+    }
+}
