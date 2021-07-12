abfa42d1ec168b3c358df819c2fecbac00753270
kalqga <kalqga123@gmail.com>
2021-07-12 13:20:24 +0300

    12.07.21

---
+++ src/for_loop/numbers_from_1_to_100.java
@@ -0,0 +1,11 @@
+package for_loop;
+
+public class numbers_from_1_to_100 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; ++i) {
+            System.out.println(i);
+        }
+
+    }
+}
